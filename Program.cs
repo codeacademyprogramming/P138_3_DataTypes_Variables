30f559586638720013795e394de3f498b967618a
EAgha <41126467+EAghayev@users.noreply.github.com>
2023-01-17 16:31:21 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number=200;
+            //int number = 200;
+            byte number = 200;
+            number = 0;
+
+            sbyte numbreSByte = -128;
+
+            short numberShort = -32000;
+
+            short number2;
+
+            number2 = 45;
+
+            bool checkNumber2 = number2 > 100;
+            checkNumber2 = true;
+            checkNumber2 = false;
+            checkNumber2 = number2<100;
+
+
+            double price = 45.5;
+
+
+
+            Console.WriteLine(number);
+
+            number = 50;
+
+            var newNumber = number + 10;
+            string name = "Hikmet";
+            string word;
+            word = "Hello";
+            char letter = 'H';
+
+
+
+
+            Console.WriteLine(name);
+
+           
+        }
+    }
+}
